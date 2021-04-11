31ba78172b4f1a05126d7a6150cd59469324a554
Lorenzo Doremi <73567993+LorenzoDoremi@users.noreply.github.com>
2021-04-11 21:37:24 +0200

    Update SplitText.js

--- SplitText/SplitText.js
+++ SplitText/SplitText.js
@@ -1,13 +1,13 @@
 import React from "react";
 import {  motion } from "framer-motion";
 
-export default function SplitText({ text, initial, animate, exit, duration, speed }) {
+export default function SplitText({ text, initial, animate, exit, duration, stagger }) {
 
   var initialS  = initial != undefined ? initial : { opacity: 0, duration: 0.5};  
   var animateS  = animate != undefined ? animate : { opacity: 1, duration: 0.5}; 
   var exitS  = exit != undefined ? exit : { opacity: 0, duration: 0.5};  
   var durationS = duration != undefined ? duration : 0.5;
-  var speedS = speedS != undefined ? duration: 0.01; 
+  var speedS = stagger != undefined ? duration: 0.01; 
   var word = Array.from(text);
 
   return word.map((value, index) => {
